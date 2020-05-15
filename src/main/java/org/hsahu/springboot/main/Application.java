9bec25bd069b8f3b4197f5e0789f5219c08f6f24
Himanshu <himanshusahu24@gmail.com>
2020-05-15 16:23:39 +0530

    added internationalization i18n for hello world controller

--- src/main/java/org/hsahu/springboot/main/Application.java
+++ src/main/java/org/hsahu/springboot/main/Application.java
@@ -6,7 +6,13 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import java.util.Locale;
 
 @Slf4j
 @SpringBootApplication
@@ -19,6 +25,25 @@ public class Application implements ApplicationRunner {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
     }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
+    }
+
+//    /**
+//     * this can also be used instead of defining message base name in application.properties
+//     * @return ResourceBundleMessageSource
+//     */
+//    @Bean
+//    public ResourceBundleMessageSource messageSource() {
+//        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+//        messageSource.setBasename("greetings");
+//        return messageSource;
+//    }
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         log.info("Application running on localhost:{}", serverPort);
