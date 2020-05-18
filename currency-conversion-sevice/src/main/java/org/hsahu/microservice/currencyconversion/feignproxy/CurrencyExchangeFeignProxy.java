52804ce75ad8e3fbe20a1a912df1945b25b2563c
Himanshu <himanshusahu24@gmail.com>
2020-05-18 16:59:03 +0530

    [currency-conversion-sevice] client side load distribution using feign rest client and ribbon name server
    1. ribbon is static name server, dynamically servers can not be added to
    ribbon server list

--- currency-conversion-sevice/src/main/java/org/hsahu/microservice/currencyconversion/feignproxy/CurrencyExchangeFeignProxy.java
+++ currency-conversion-sevice/src/main/java/org/hsahu/microservice/currencyconversion/feignproxy/CurrencyExchangeFeignProxy.java
@@ -1,11 +1,13 @@
 package org.hsahu.microservice.currencyconversion.feignproxy;
 
 import org.hsahu.microservice.currencyconversion.ConvertedCurrency;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+@FeignClient(name = "currency-exchange-service")
+@RibbonClient(name = "currency-exchange-service") // use ribbon for load distribution, this name must match with application.properties file
 public interface CurrencyExchangeFeignProxy {
 
     @GetMapping("/currency-exchange/from/{source_currency}/to/{destination_currency}")
